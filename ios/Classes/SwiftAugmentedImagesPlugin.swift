3ef6f18b264d9b916405452c6facbc6acb31f18e
Nikolaus Schauersberger <nikolaus.schauersberger@gmail.com>
2022-02-09 11:23:33 +0100

    Add native view for iOS

--- ios/Classes/SwiftAugmentedImagesPlugin.swift
+++ ios/Classes/SwiftAugmentedImagesPlugin.swift
@@ -6,6 +6,9 @@ public class SwiftAugmentedImagesPlugin: NSObject, FlutterPlugin {
     let channel = FlutterMethodChannel(name: "augmented_images", binaryMessenger: registrar.messenger())
     let instance = SwiftAugmentedImagesPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+
+    let factory = FLNativeViewFactory(messenger: registrar.messenger())
+    registrar.register(factory, withId: "com.schauersberger.augmentedimgs/cameraview")
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
--- lib/camera_view.dart
+++ lib/camera_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 
 class AugmentedCameraView extends StatefulWidget {
   const AugmentedCameraView({Key? key}) : super(key: key);
@@ -29,31 +30,42 @@ class CameraViewState extends State<AugmentedCameraView> {
   }
 
   Widget getCameraView() {
-    return PlatformViewLink(
-      viewType: viewType,
-      surfaceFactory:
-          (BuildContext context, PlatformViewController controller) {
-        return AndroidViewSurface(
-          controller: controller as AndroidViewController,
-          gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
-          hitTestBehavior: PlatformViewHitTestBehavior.opaque,
-        );
-      },
-      onCreatePlatformView: (PlatformViewCreationParams params) {
-        return PlatformViewsService.initSurfaceAndroidView(
-          id: params.id,
-          viewType: viewType,
-          layoutDirection: TextDirection.ltr,
-          creationParams: creationParams,
-          creationParamsCodec: const StandardMessageCodec(),
-          onFocus: () {
-            params.onFocusChanged(true);
-          },
-        )
-          ..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)
-          ..create();
-      },
-    );
+    if (Platform.isAndroid) {
+      return PlatformViewLink(
+        viewType: viewType,
+        surfaceFactory:
+            (BuildContext context, PlatformViewController controller) {
+          return AndroidViewSurface(
+            controller: controller as AndroidViewController,
+            gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+            hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+          );
+        },
+        onCreatePlatformView: (PlatformViewCreationParams params) {
+          return PlatformViewsService.initSurfaceAndroidView(
+            id: params.id,
+            viewType: viewType,
+            layoutDirection: TextDirection.ltr,
+            creationParams: creationParams,
+            creationParamsCodec: const StandardMessageCodec(),
+            onFocus: () {
+              params.onFocusChanged(true);
+            },
+          )
+            ..addOnPlatformViewCreatedListener(params.onPlatformViewCreated)
+            ..create();
+        },
+      );
+    } else if (Platform.isIOS) {
+      return const UiKitView(
+        viewType: viewType,
+        layoutDirection: TextDirection.ltr,
+        creationParams: creationParams,
+        creationParamsCodec: StandardMessageCodec(),
+      );
+    } else {
+      return const Text('This platform is not supported');
+    }
   }
 
 }
